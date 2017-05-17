cef4980273cd34775bf4435e47f6a4a1eb4b506b
gyamin <n.gyamin@gmail.com>
2017-05-17 10:48:36 +0900

    Vue.js コンポーネント 利用例2

--- vue_test/webpack.config.js
+++ vue_test/webpack.config.js
@@ -9,7 +9,9 @@ module.exports = [
             "test3/index": './src/javascript/test3/entry.js',
             "test4/index": './src/javascript/test4/entry.js',
             "test5/index": './src/javascript/test5/entry.js',
-            "test6/index": './src/javascript/test6/entry.js'
+            "test6/index": './src/javascript/test6/entry.js',
+            "test7/index": './src/javascript/test7/entry.js',
+            "test8/index": './src/javascript/test8/entry.js'
         },
         output: {
             path: __dirname + '/dist/javascripts/',
